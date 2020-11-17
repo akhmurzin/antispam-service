dbcc4f50163026d3ddf631c3d2edb9a909bb59c7
Adilzhan Akhmurzin <adilzhan.akhmurzin@nu.edu.kz>
2020-11-17 21:46:07 +0600

    Не работающий запрос

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 header('Content-type: application/json; charset=utf-8');
 echo json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
+
+require_once('is_spam.php');
+
+$norm = normalize($_POST['text']);
+if (!empty($_POST)) {
+
+}
