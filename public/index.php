5cc2f21657ec1e374215f13404f4ea663d200662
Adilzhan Akhmurzin <adilzhan.akhmurzin@nu.edu.kz>
2020-11-20 18:24:44 +0600

    added HTTP Request file, deleted redundant newline in stopwords.txt, updated index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,77 @@
 <?php
 header('Content-type: application/json; charset=utf-8');
-echo json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
+//echo json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
 
-require_once('is_spam.php');
+function process_file($path) {
+    $wordArray = explode("\n", file_get_contents($path));
+    foreach ($wordArray as &$eachWord) {
+        $eachWord = trim($eachWord);
+    }
+    unset($eachWord);
+    return $wordArray;
+}
 
-$norm = normalize($_POST['text']);
+function normalize($post) {
+    if (!empty($post["text"])) {
+        $txt = $post["text"];
+    }
+    $tokensArray = array();
+    $tok = strtok($txt, " .,!?[]()<>:;-\n'\r\"/*|");
+    while ($tok !== false) {
+        array_push($tokensArray, $tok);
+        $tok = strtok(" .,!?[]()<>:;-\n'\r\"/*|");
+    }
+
+    foreach ($tokensArray as &$eachToken) {
+        $eachToken = mb_strtolower($eachToken);
+    }
+    unset($eachToken);
+//    echo "tokensArray to lower case:\n";
+//    var_dump($tokensArray);
+
+//    $stopWords = explode("\n", file_get_contents('/code/docs/stopwords.txt'));
+//
+//    foreach ($stopWords as &$eachWord) {
+//        $eachWord = trim($eachWord);
+//    }
+//    unset($eachWord);
+//    echo "stopWords without spaces:\n";
+//    var_dump($stopWords);
+
+    $stopWords = process_file('/code/docs/stopwords.txt');
+//    echo "stopWords without spaces:\n";
+//    var_dump($stopWords);
+
+    $tokensArray = array_diff($tokensArray, $stopWords);
+//    echo "После удаления стоп слов:\n";
+//    var_dump($tokensArray);
+
+    $tokensArray = preg_grep('/\d+/', $tokensArray, PREG_GREP_INVERT);
+//    echo "Минус слова из чисел:\n";
+//    var_dump($tokensArray);
+
+    sort($tokensArray);
+    echo "Отсортированный массив:\n";
+    var_dump($tokensArray);
+
+    return $tokensArray;
+}
+
+function block_list($checkArray) {
+    $blockWords = process_file('/code/docs/blocklist.txt');
+    var_dump($blockWords);
+    $blockFound = (bool)count(array_intersect($blockWords, $checkArray));
+    var_dump($blockFound);
+    if ($blockFound) {
+//        return "Block word is found";
+        return ['status' => 'ok', 'spam' => 'true', 'reason' => 'block_list', 'normalized_text' => "placeholder for array"];
+    }
+}
+
 if (!empty($_POST)) {
-
+    $normalizedArray = normalize($_POST);
+    $isSpam = block_list($normalizedArray);
+    echo json_encode($isSpam);
+} else {
+    echo "\nArray is empty";
 }
